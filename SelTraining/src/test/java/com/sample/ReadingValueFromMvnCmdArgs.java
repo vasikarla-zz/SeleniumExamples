1e030cbeb898a5f0ffdde5cbfbdada3f5fcd204e
rvasikarla <raj_vasikarla@intuit.com>
2015-02-19 23:12:45 -0800

    added few more examples

---
+++ SelTraining/src/test/java/com/sample/ReadingValueFromMvnCmdArgs.java
@@ -0,0 +1,14 @@
+package com.sample;
+
+import org.testng.annotations.Test;
+
+public class ReadingValueFromMvnCmdArgs {
+
+	@Test
+	public void HowToReadValuesFromMvnCmdLine() {
+		System.out.println("This is sample text");
+		System.out.println(System.getProperty("buildVersion",
+				"This is the default value"));
+		System.out.println(System.getenv("buildVersion"));
+	}
+}
